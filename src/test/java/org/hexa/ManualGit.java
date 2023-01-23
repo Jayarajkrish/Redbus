14e56f7bbe06c1cb7243b2ae545174f73363a142
vijay anand <Jayarajmca21@gmail.com>
2023-01-23 13:32:58 +0530

    update fron Sivabalan

--- src/test/java/org/hexa/ManualGit.java
+++ src/test/java/org/hexa/ManualGit.java
@@ -16,5 +16,14 @@ public class ManualGit {
 	System.out.println("TmailNadu");
 	}
 	
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	
 
 }
